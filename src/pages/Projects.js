7444dca180c7ede74db4de2d2ae98289462b394e
Jake Kaplan <kaplanjt@gmail.com>
2021-11-06 12:34:04 -0700

    frontend complete

---
+++ src/pages/Projects.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+function Projects(props) {
+
+    const [projects, setProjects] = useState(null)
+
+    const getProjectsData = async () => {
+        const response = await fetch(props.URL + "projects");
+        const data = await response.json();
+        setProjects(data);
+    };
+
+    useEffect(() => getProjectsData(), []);
+
+    const loaded = () => {
+        return projects.map((project) => (
+            <div>
+                <h1>{project.name}</h1>
+                <img src={project.image} />
+                <a href={project.git}>
+                    <button>Github</button>
+                </a>
+                <a href={project.live}>
+                    <buttton>Live Site</buttton>
+                </a>
+            </div>
+        ));
+    }
+
+    return projects ? loaded() : <h1>Loading....</h1>
+}
+export default Projects
